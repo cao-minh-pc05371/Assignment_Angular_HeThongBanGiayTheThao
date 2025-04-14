fa29f141f70341024b16d99ee08895b2fc085cc3
cao-minh-pc05371 <minhncpc05371@fpt.edu.vn>
2025-04-14 16:30:00 +0700

    danh sách, chức năng CRUD Size

--- src/app/pages/Admin/variant/size_variation/add-size/add-size.component.ts
+++ src/app/pages/Admin/variant/size_variation/add-size/add-size.component.ts
@@ -1,11 +1,58 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { ISize } from 'src/app/interface/size.interface';
+import { SizeService } from 'src/app/services/apis/size.service';
 
 @Component({
   selector: 'app-add-size',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './add-size.component.html',
   styleUrl: './add-size.component.scss'
 })
 export class AddSizeComponent {
+  formData = new FormGroup({
+    size_label: new FormControl('', [Validators.required])
+  });
 
+  get size_label() {
+    return this.formData.get('size_label');
+  }
+
+  constructor(
+    private sizeService: SizeService,
+    private router: Router
+  ) { }
+
+  addSize() {
+    if (this.formData.invalid) {
+      return;
+    }
+
+    const size: ISize = {
+      id: 0,
+      size_label: this.formData.value.size_label ?? ''
+    };
+
+    this.sizeService.addSize(size).subscribe({
+      next: () => {
+        this.router.navigate(['/admin/size_variation/list-size-variant']);
+      },
+      error: (err) => {
+        console.error('Thêm thất bại:', err);
+      }
+    });
+  }
 }
--- src/app/pages/Admin/variant/size_variation/list-size/list-size.component.ts
+++ src/app/pages/Admin/variant/size_variation/list-size/list-size.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+import { ISize } from 'src/app/interface/size.interface';
+import { SizeService } from 'src/app/services/apis/size.service';
+import { DeleteComponent } from '../delete/delete.component';
 
 @Component({
   selector: 'app-list-size',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, CommonModule],
   templateUrl: './list-size.component.html',
   styleUrl: './list-size.component.scss'
 })
 export class ListSizeComponent {
+  readonly dialog = inject(MatDialog);
 
+  size: ISize[] = [];
+  constructor(
+    private sizeService: SizeService,
+    private route: Router,
+  ) { 
+    this.getAllSize();
+  }
+
+  getAllSize() {
+    this.sizeService.getAllSize().subscribe({
+      next: (res: any) => {
+        this.size = res?.data ?? res;
+        console.log('Size:', this.size);
+      },
+      error: (err) => {
+        console.error('Error fetching size:', err);
+      }
+    })
+  }
+
+  openDialog(id: number, name: string): void {
+    const dialogRef = this.dialog.open(DeleteComponent, {
+      data: {name: name, id: id},
+
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+     if (result) {
+        this.getAllSize();
+      }
+     
+    });
+  }
+
+  openEditDialog(id: number, name: string) {
+    this.route.navigate(['/admin/size_variation/edit-size-variant', id]);
+  }
 }
+
