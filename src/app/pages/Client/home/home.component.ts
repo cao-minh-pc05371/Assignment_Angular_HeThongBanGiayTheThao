93c2efa712e8f62f9ec5b8c6517e0ef559e4a36a
tritrpc07133 <tritrpc07133@fpt.edu.vn>
2025-04-18 15:06:06 +0700

    cập nhận lại trang home

--- src/app/pages/Client/home/home.component.ts
+++ src/app/pages/Client/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { CategoryService } from 'src/app/services/apis/category.service';
@@ -18,27 +18,54 @@ import { IBrands } from 'src/app/interface/brands.interface';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  banners = [
+    {
+      image: 'https://media.licdn.com/dms/image/C5112AQF5Zq8eziJ7Cw/article-cover_image-shrink_600_2000/0/1520150129347?e=2147483647&v=beta&t=o_6S9A6VB6IbwzfRZFNW8xW4dgwONkl9zdZvcmE3cxI',
+      alt: 'Banner 1',
+      title: 'Khuyến mãi mùa hè',
+      description: 'Giảm giá lên đến 50%'
+    },
+    {
+      image: 'https://lambanner.com/wp-content/uploads/2022/10/MNT-DESIGN-BANNER-GIAY-11.jpg',
+      alt: 'Banner 2',
+      title: 'Bộ sưu tập mới',
+      description: 'Cập nhật những mẫu mã mới nhất'
+    },
+    {
+      image: 'https://i.ytimg.com/vi/88g98VcVIko/maxresdefault.jpg',
+      alt: 'Banner 3',
+      title: 'Sản phẩm bán chạy',
+      description: 'Những sản phẩm được ưa chuộng nhất'
+    }
+  ];
+
   list: ICategories[] = [];
   listProduct: IProduct[] = [];
   listBrand: IBrands[] = [];
+  featuredProducts: IProduct[] = [];
+  saleProducts: IProduct[] = [];
 
   constructor(
     private categoryService: CategoryService,
     private productService: ProductService,
-    private brandService: BrandService
+    private brandService: BrandService,
+    public router: Router
   ) {}
 
   ngOnInit(): void {
-    this.getData();
-    this.getProduct();
+    this.loadAllData();
+  }
+
+  loadAllData(): void {
+    this.getCategories();
+    this.getProducts();
     this.getBrands();
   }
 
-  getData(): void {
+  getCategories(): void {
     this.categoryService.getCategories().subscribe({
       next: (res: any) => {
         this.list = res?.data ?? res;
-        // this.list = data.filter((item: any) => item.status === 'visible');
       },
       error: (err) => {
         console.error('Error fetching categories:', err);
@@ -46,11 +73,12 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  getProduct(): void {
+  getProducts(): void {
     this.productService.getProducts().subscribe({
       next: (res: any) => {
         this.listProduct = res?.data ?? res;
-        // this.listProduct = data.filter((item: any) => item.status === 'visible');
+        this.filterFeaturedProducts();
+        this.filterSaleProducts();
       },
       error: (err) => {
         console.error('Error fetching products:', err);
@@ -62,11 +90,38 @@ export class HomeComponent implements OnInit {
     this.brandService.getAllBrands().subscribe({
       next: (res: any) => {
         this.listBrand = res?.data ?? res;
-        // this.listBrand = data.filter((item: any) => item.status === 'visible');
       },
       error: (err) => {
         console.error('Error fetching brands:', err);
       }
     });
+  }
+
+  private filterFeaturedProducts(): void {
+    this.featuredProducts = this.listProduct
+      .filter(product => product.featured || product.rating >= 4)
+      .slice(0, 12);
+  }
+
+  private filterSaleProducts(): void {
+    this.saleProducts = this.listProduct
+      .filter(product => product.discount > 0)
+      .sort((a, b) => b.discount - a.discount)
+      .slice(0, 12);
+  }
+  calculatePrice(originalPrice: number, discount: number): { discountedPrice: number, originalPrice: number } {
+    const discountedPrice = discount > 0 ? originalPrice * (1 - discount / 100) : originalPrice;
+    return {
+      discountedPrice: discountedPrice,
+      originalPrice: originalPrice
+    };
+  }
+
+
+  formatCurrency(value: number): string {
+    return new Intl.NumberFormat('vi-VN', {
+      style: 'currency',
+      currency: 'VND'
+    }).format(value);
   }
 }
